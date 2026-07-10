9c9d820a953d420b86ccbb6acac3633a8454d456
Kobashigawa <u202412921@upc.edu.pe>
2026-07-09 23:44:57 -0500

    Delete public/assets/styles/menu-toggle.js

--- public/assets/styles/menu-toggle.js
+++
@@ -1,5 +0,0 @@
-const btn = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".Horizontal-Menu");
-btn.addEventListener("click", () => {
-  menu.classList.toggle("active");
-});
